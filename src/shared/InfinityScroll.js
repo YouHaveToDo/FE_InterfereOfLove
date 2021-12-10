876fb5b8feeda28abbb523d917a3bef5cb868be7
가즈아 <wjsdydxo45@naver.com>
2021-12-11 03:19:44 +0900

    omg

---
+++ src/shared/InfinityScroll.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, useCallback } from "react";
+import _ from 'lodash';
+import Loader from './Loader';
+
+const InfiniteScroll = (props) => {
+  const { children, callNextPage, isNext, isLoading } = props;
+
+  const _handleScroll = _.throttle(() => {
+    if (isLoading) {
+      return;
+    }
+
+    const { innerHeight } = window;
+    const { scrollHeight } = document.body;
+
+    const scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+
+    if (scrollHeight - innerHeight - scrollTop < 200) {
+      callNextPage();
+    }
+  }, 1200);
+
+  const handleScroll = React.useCallback(_handleScroll, [isLoading]);
+
+  React.useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (isNext) {
+      window.addEventListener('scroll', handleScroll);
+    } else {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isNext, isLoading]);
+
+  return (
+    <React.Fragment>
+      {children}
+      {isNext && <Loader />}
+    </React.Fragment>
+  );
+};
+
+InfiniteScroll.defaultProps = {
+  children: null,
+  callNextPage: () => {},
+  isNext: false,
+  isLoading: false,
+};
+
+export default InfiniteScroll;
